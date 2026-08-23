b12f1836b3f1372723aab41bd5c92a9278b95b50
luna 3 <93050744+astranema@users.noreply.github.com>
2026-08-23 00:39:05 -0400

    Add logic for finding subtype and type (ESCP, E/P)

--- results.js
+++ results.js
@@ -11,11 +11,31 @@ const sociabilityDisplay = document.getElementById("sociability-display");
 const efficacyDisplay = document.getElementById("efficacy-display");
 const prosocialityDisplay = document.getElementById("prosociality-display");
 
+function getStack(c, s, e, p) {
+    let stack = "";
+    let aspectsArr = ["C", "S", "E", "P"];
+    let valuesArr = [c, s, e, p];
+    for (let i = 0; i < 4; i++) {
+        maxIndex = 0;
+        for (let j = 0; j < (4 - i); j++) {
+            if (valuesArr[j] > valuesArr[maxIndex]) {
+                maxIndex = j;
+            }
+        }
+        stack += aspectsArr[maxIndex];
+        aspectsArr.splice(maxIndex, 1);
+        valuesArr.splice(maxIndex, 1);
+    }
+    return stack;
+}
+
 function main() {
-    curiosityDisplay.textContent = `Curiosity: ${curiosity}`
-    sociabilityDisplay.textContent = `Sociability: ${sociability}`
-    efficacyDisplay.textContent = `Efficacy: ${efficacy}`
-    prosocialityDisplay.textContent = `Prosociality: ${prosociality}`
+    curiosityDisplay.textContent = `Curiosity: ${curiosity}`;
+    sociabilityDisplay.textContent = `Sociability: ${sociability}`;
+    efficacyDisplay.textContent = `Efficacy: ${efficacy}`;
+    prosocialityDisplay.textContent = `Prosociality: ${prosociality}`;
+    const subtype = getStack(curiosity, sociability, efficacy, prosociality);
+    const type = subtype[0] + "/" + subtype[3];
 }
 
 main();
